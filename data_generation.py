3cd1fddbd92cfa5d5af185dc00e4c3c3650f00e1
Suyoscar11 <suyoscar11@gmail.com>
2025-06-05 08:13:12 -0500

    Starting files for random distributed test data

---
+++ data_generation.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+
+# Parameters for Gaussian distribution
+
+mean = 0
+std_dev = 1
+size = 1000
+
+# Generate data
+data_gaussian = np.random.normal(loc=mean, scale=std_dev, size=size)
