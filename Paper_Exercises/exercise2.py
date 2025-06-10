92d7deeaf0d465147ce32b62ca32e2002028ec1e
Suyoscar11 <suyoscar11@gmail.com>
2025-06-09 20:03:10 -0500

    completed exs 2, just used the datasets properly

--- Paper_Exercises/exercise2.py
+++ Paper_Exercises/exercise2.py
@@ -1,29 +1,16 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-# x_values = [203, 58, 210, 202, 198, 158, 165, 201, 157, 131, 166, 160, 186, 125, 218, 146]
-# y_values = [495, 173, 479, 504, 510, 416, 393, 442, 317, 311, 400, 337, 423, 334, 533, 344]
-# sigma_y  = [21, 15, 27, 14, 30, 16, 14, 25, 52, 16, 34, 31, 42, 26, 16, 22]
+import numpy as np
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("Paper_Exercises\data.csv")
 
 
-# Drop the first four rows
-exs1_data = data.iloc[4:]
+x= data["x"].values
+y = data["y"].values
+sigma_y=data["sigma_y"].values
+sigma_x=data["sigma_x"].values
+rho_xy= data["rho_xy"].values
 
-final_data= exs1_data.drop(["sigma_x", "rho_xy"], axis=1)
-print(final_data)
-
-
-#This .values will return a numpy array
-x= final_data["x"].values
-y= final_data["y"].values
-sigma_y = final_data["sigma_y"].values
-
-
-#An interesting thing I found out while writing this code, so numpy has smthg like (N,1), where N is row and 1 is column, SInce according to the problem
-#statement we need a column vector of Y, so the positional argument at index 2 will be column, and assigning -1 will automatically set up rows in numpy
 Y = y.reshape(-1,1)
 
 
